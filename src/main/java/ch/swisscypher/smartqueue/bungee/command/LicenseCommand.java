21ed189f59c7a418a6f8da6c3f9667bbb9780d97
Zayceur <zayceur@gmail.com>
2021-02-24 22:44:51 +0100

    Bump api version + apply license to new files

--- src/main/java/ch/swisscypher/smartqueue/bungee/command/LicenseCommand.java
+++ src/main/java/ch/swisscypher/smartqueue/bungee/command/LicenseCommand.java
@@ -1,3 +1,20 @@
+/*
+ * SmartQueue: Minecraft plugin implementing a queue system.
+ * Copyright (C) 2021 Zayceur (dev@zayceur.ch)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package ch.swisscypher.smartqueue.bungee.command;
 
 import ch.swisscypher.smartqueue.common.util.LicenseManager;
